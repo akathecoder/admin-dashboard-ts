d387bd28a57e5d9ddad37196c7f8969d00402ede
Sparsh Agarwal <sparshaga25@gmail.com>
2021-05-18 16:15:41 +0530

    Updated AppBar Color

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,9 +1,28 @@
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, makeStyles, Toolbar, Typography } from '@material-ui/core';
 import React from 'react';
 
+const useStyles = makeStyles({
+    appBarColor: {
+        background: 'white',
+        boxShadow: '0 3px 5px 2px rgba(0, 0, 0, .1)',
+    },
+    label: {
+        textTransform: 'capitalize',
+    },
+});
+
 const Appbar: React.FC = () => {
+    const classes = useStyles();
+
     return (
-        <AppBar position="fixed">
+        <AppBar
+            position="fixed"
+            color="primary"
+            elevation={1}
+            classes={{
+                colorPrimary: classes.appBarColor,
+            }}
+        >
             <Toolbar>
                 <Typography variant="h6" noWrap>
                     Admin Dashboard
