f9e7067de2043a22c82d90daa779d4cb93af9b22
Sparsh Agarwal <sparshaga25@gmail.com>
2021-05-18 15:57:23 +0530

    Added Styles to Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import { Divider, Drawer, List, ListItem, ListItemIcon, ListItemText, SvgIconProps } from '@material-ui/core';
+import {
+    Divider,
+    Drawer,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    SvgIconProps,
+    Typography,
+} from '@material-ui/core';
 import { variables } from '../assets/themes/variables';
 import {
     PermIdentityOutlined,
@@ -12,6 +21,7 @@ import {
     MailOutlineOutlined,
     MoreHorizOutlined,
 } from '@material-ui/icons';
+import { COLORS } from '../assets/themes/colors';
 
 const drawerWidth = variables.drawerWidth;
 
@@ -26,8 +36,24 @@ const useStyles = makeStyles((theme: Theme) =>
         },
         // necessary for content to be below app bar
         toolbar: theme.mixins.toolbar,
+        drawerToolbar: {
+            display: 'table',
+            paddingLeft: '1rem',
+        },
         drawerTitle: {
-            textAlign: 'center',
+            textAlign: 'left',
+            verticalAlign: 'middle',
+            display: 'table-cell',
+            fontWeight: 500,
+            color: COLORS.AccentBlue,
+        },
+        drawerList: {
+            marginTop: '1rem',
+            marginBottom: '1rem',
+        },
+        icon: {
+            minWidth: 'unset',
+            marginRight: '1rem',
         },
     }),
 );
@@ -74,26 +100,31 @@ export const Sidebar: React.FC = () => {
                 paper: classes.drawerPaper,
             }}
             anchor="left"
+            PaperProps={{ variant: 'elevation', elevation: 16 }}
         >
-            <div className={classes.toolbar}>
-                <div className={classes.drawerTitle}>Admin Dashboard</div>
+            <div className={`${classes.toolbar} ${classes.drawerToolbar}`}>
+                <Typography variant="h6" noWrap className={classes.drawerTitle}>
+                    Admin Dashboard
+                </Typography>
             </div>
             <Divider />
-            <List>
+            <List className={classes.drawerList}>
                 {DrawerItems.map((item, index) => (
                     <ListItem button key={item.name}>
-                        <ListItemIcon>{item.icon}</ListItemIcon>
+                        <ListItemIcon className={classes.icon}>{item.icon}</ListItemIcon>
                         <ListItemText primary={item.name} key={index} />
                     </ListItem>
                 ))}
             </List>
             <Divider />
-            <ListItem button>
-                <ListItemIcon>
-                    <MoreHorizOutlined />
-                </ListItemIcon>
-                <ListItemText primary="Settings" />
-            </ListItem>
+            <List className={classes.drawerList}>
+                <ListItem button>
+                    <ListItemIcon className={classes.icon}>
+                        <MoreHorizOutlined />
+                    </ListItemIcon>
+                    <ListItemText primary="Settings" />
+                </ListItem>
+            </List>
         </Drawer>
     );
 };
