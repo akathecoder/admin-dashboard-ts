6ffcd4941660c622e85c25f34a43b2264c925e47
Sparsh Agarwal <sparshaga25@gmail.com>
2021-05-19 21:34:38 +0530

    Updated Table Head color to white

--- src/components/UserDataTable.tsx
+++ src/components/UserDataTable.tsx
@@ -1,10 +1,21 @@
 import { makeStyles, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
 import React from 'react';
 import { USER } from '../models/firestoreModel';
+import { COLORS } from '../assets/themes/colors';
 
 const useStyles = makeStyles({
     container: {
         maxHeight: '80vh',
+        boxShadow: '0 8px 16px 0 rgba(0,0,0,0.2)',
+    },
+    tableHeadBgColor: {
+        backgroundColor: `${COLORS.White}`,
+    },
+    tableRowHover: {
+        '&:hover': {
+            boxShadow: '0 2px 6px 0 rgba(0,0,0,0.2)',
+            backgroundColor: `${COLORS.White} !important`,
+        },
     },
 });
 
@@ -22,15 +33,45 @@ const DataTable: React.FC<DataTableProps> = ({ dataBody }: DataTableProps) => {
                 <Table stickyHeader>
                     <TableHead>
                         <TableRow>
-                            <TableCell>Name</TableCell>
-                            <TableCell>Email</TableCell>
-                            <TableCell>Role</TableCell>
-                            <TableCell>Recent Activity</TableCell>
+                            <TableCell
+                                classes={{
+                                    stickyHeader: classes.tableHeadBgColor,
+                                }}
+                            >
+                                Name
+                            </TableCell>
+                            <TableCell
+                                classes={{
+                                    stickyHeader: classes.tableHeadBgColor,
+                                }}
+                            >
+                                Email
+                            </TableCell>
+                            <TableCell
+                                classes={{
+                                    stickyHeader: classes.tableHeadBgColor,
+                                }}
+                            >
+                                Role
+                            </TableCell>
+                            <TableCell
+                                classes={{
+                                    stickyHeader: classes.tableHeadBgColor,
+                                }}
+                            >
+                                Recent Activity
+                            </TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {dataBody.map((row, index) => (
-                            <TableRow key={index}>
+                            <TableRow
+                                hover
+                                key={index}
+                                classes={{
+                                    hover: classes.tableRowHover,
+                                }}
+                            >
                                 <TableCell>{row.name}</TableCell>
                                 <TableCell>{row.email}</TableCell>
                                 <TableCell>{row.role}</TableCell>
