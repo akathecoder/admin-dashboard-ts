7d3d80405401e2aabf267473c69b68e9691bb9e0
Sparsh Agarwal <sparshaga25@gmail.com>
2021-06-09 20:13:53 +0530

    Added formik validation to Add Member Dialog

--- src/components/Member/AddMemberDialog.tsx
+++ src/components/Member/AddMemberDialog.tsx
@@ -10,8 +10,11 @@ import {
     TextField,
     Theme,
 } from '@material-ui/core';
-import React, { useState } from 'react';
+import React from 'react';
+import { useFormik, FormikHelpers } from 'formik';
+import * as yup from 'yup';
 import { createMember } from '../../utils/memberFunctions';
+import { MEMBER } from '../../models/firestoreModel';
 
 interface AddMemberDialogProps {
     open: boolean;
@@ -28,22 +31,46 @@ const useStyles = makeStyles((theme: Theme) =>
     }),
 );
 
+const validationSchema = yup.object({
+    name: yup.string().required('Name is required'),
+    phone: yup.number().required('Phone number is required'),
+    gender: yup.string().oneOf(['male', 'female'], 'Gender is ').required('Gender is required'),
+    email: yup.string().email('Enter a valid email').required('Email is required'),
+    college: yup.string().required('Email is required'),
+    currentProfession: yup.string().required('Current Profession is required'),
+    uid: yup.string().required('UID is required'),
+    yearOfPassing: yup.number().required('Year of Passing is required'),
+    profileImage: yup.string().url('Enter a valid Profile Picture').optional(),
+    address: yup.string().optional(),
+});
+
+const initialValues: MEMBER = {
+    name: '',
+    phone: 0,
+    gender: '',
+    email: '',
+    college: '',
+    currentProfession: '',
+    uid: '',
+    yearOfPassing: 0,
+    profileImage: '',
+    address: '',
+};
+
 const AddMemberDialog: React.FC<AddMemberDialogProps> = ({ open, onClose }: AddMemberDialogProps) => {
     const classes = useStyles();
 
-    // Member Data States
-    const [name, setName] = useState('');
-    const [phone, setPhone] = useState<number | null>(null);
-    const [profileImage, setProfileImage] = useState('');
-    const [gender, setGender] = useState('');
-    const [email, setEmail] = useState('');
-    const [college, setCollege] = useState('');
-    const [currentProfession, setCurrentProfession] = useState('');
-    const [uid, setUid] = useState('');
-    const [yearOfPassing, setYearOfPassing] = useState<number | null>(null);
-    const [address, setAddress] = useState('');
+    const handleClose = () => {
+        formik.resetForm();
+        onClose();
+    };
 
-    const handleSubmit = () => {
+    const handleSubmit = (values: MEMBER, { setSubmitting, resetForm }: FormikHelpers<MEMBER>) => {
+        console.log(values);
+
+        const { name, phone, gender, email, college, currentProfession, uid, yearOfPassing, profileImage, address } =
+            values;
+
         if (!name || !phone || !gender || !email || !college || !currentProfession || !uid || !yearOfPassing) {
             return;
         }
@@ -60,127 +87,148 @@ const AddMemberDialog: React.FC<AddMemberDialogProps> = ({ open, onClose }: AddM
             profileImage,
             address,
         ).then(() => {
+            setSubmitting(false);
+            resetForm();
             onClose();
         });
     };
 
+    const formik = useFormik({
+        initialValues: initialValues,
+        validationSchema: validationSchema,
+        onSubmit: handleSubmit,
+    });
+
     return (
-        <Dialog onClose={onClose} open={open}>
+        <Dialog onClose={handleClose} open={open}>
             <DialogTitle id="simple-dialog-title">Add Member</DialogTitle>
             <DialogContent>
                 <DialogContentText>Fill in the data for the new member.</DialogContentText>
 
                 <form noValidate autoComplete="off" className={classes.root}>
                     <TextField
+                        fullWidth
+                        variant="standard"
                         id="newMemberProfilePic"
+                        name="profileImage"
                         label="Profile Picture"
-                        helperText="Please enter user's DP"
-                        variant="standard"
-                        value={profileImage}
-                        onChange={(e) => setProfileImage(e.target.value)}
-                        fullWidth
+                        value={formik.values.profileImage}
+                        onChange={formik.handleChange}
+                        error={formik.touched.profileImage && Boolean(formik.errors.profileImage)}
+                        helperText={formik.touched.profileImage && formik.errors.profileImage}
                     />
+
                     <TextField
+                        fullWidth
+                        variant="standard"
                         id="newMemberUid"
+                        name="uid"
                         label="UID"
-                        helperText="Please enter user's uid"
-                        variant="standard"
-                        value={uid}
-                        onChange={(e) => setUid(e.target.value)}
-                        required
-                        fullWidth
+                        value={formik.values.uid}
+                        onChange={formik.handleChange}
+                        error={formik.touched.uid && Boolean(formik.errors.uid)}
+                        helperText={formik.touched.uid && formik.errors.uid}
                     />
+
                     <TextField
+                        fullWidth
+                        variant="standard"
                         id="newMemberName"
+                        name="name"
                         label="Name"
-                        helperText="Please enter user's name"
-                        variant="standard"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        required
-                        fullWidth
+                        value={formik.values.name}
+                        onChange={formik.handleChange}
+                        error={formik.touched.name && Boolean(formik.errors.name)}
+                        helperText={formik.touched.name && formik.errors.name}
                     />
 
                     <TextField
+                        fullWidth
+                        variant="standard"
                         id="newMemberPhone"
-                        label="Phone"
-                        type="number"
-                        helperText="Please enter user's phone number"
-                        variant="standard"
-                        value={phone}
-                        onChange={(e) => setPhone(Number(e.target.value))}
-                        required
-                        fullWidth
+                        name="phone"
+                        label="Phone Number"
+                        value={formik.values.phone}
+                        onChange={formik.handleChange}
+                        error={formik.touched.phone && Boolean(formik.errors.phone)}
+                        helperText={formik.touched.phone && formik.errors.phone}
                     />
 
                     <TextField
+                        fullWidth
+                        variant="standard"
                         id="newMemberGender"
+                        name="gender"
                         label="Gender"
-                        helperText="Please select user's Gender"
-                        variant="standard"
-                        value={gender}
-                        onChange={(e) => setGender(e.target.value)}
-                        required
-                        fullWidth
+                        value={formik.values.gender}
+                        onChange={formik.handleChange}
+                        error={formik.touched.gender && Boolean(formik.errors.gender)}
+                        helperText={formik.touched.gender && formik.errors.gender}
                     />
 
                     <TextField
+                        fullWidth
+                        variant="standard"
                         id="newMemberEmail"
+                        name="email"
                         label="Email ID"
-                        helperText="Please enter user's email id"
-                        variant="standard"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                        fullWidth
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        error={formik.touched.email && Boolean(formik.errors.email)}
+                        helperText={formik.touched.email && formik.errors.email}
                     />
 
                     <TextField
+                        fullWidth
+                        variant="standard"
                         id="newMemberCollege"
+                        name="college"
                         label="College"
-                        helperText="Please enter user's college"
-                        variant="standard"
-                        value={college}
-                        onChange={(e) => setCollege(e.target.value)}
-                        required
-                        fullWidth
+                        value={formik.values.college}
+                        onChange={formik.handleChange}
+                        error={formik.touched.college && Boolean(formik.errors.college)}
+                        helperText={formik.touched.college && formik.errors.college}
                     />
 
                     <TextField
+                        fullWidth
+                        variant="standard"
                         id="newMemberCurrentProfession"
+                        name="currentProfession"
                         label="Current Profession"
-                        helperText="Please enter user's current profession"
-                        variant="standard"
-                        value={currentProfession}
-                        onChange={(e) => setCurrentProfession(e.target.value)}
-                        required
-                        fullWidth
+                        value={formik.values.currentProfession}
+                        onChange={formik.handleChange}
+                        error={formik.touched.currentProfession && Boolean(formik.errors.currentProfession)}
+                        helperText={formik.touched.currentProfession && formik.errors.currentProfession}
                     />
 
                     <TextField
+                        fullWidth
+                        variant="standard"
                         id="newMemberYearOfPassing"
+                        name="yearOfPassing"
                         label="Year of Passing"
-                        helperText="Please enter user's year of Passing"
-                        variant="standard"
-                        value={yearOfPassing}
-                        onChange={(e) => setYearOfPassing(Number(e.target.value))}
-                        required
-                        fullWidth
+                        value={formik.values.yearOfPassing}
+                        onChange={formik.handleChange}
+                        error={formik.touched.yearOfPassing && Boolean(formik.errors.yearOfPassing)}
+                        helperText={formik.touched.yearOfPassing && formik.errors.yearOfPassing}
                     />
 
                     <TextField
-                        id="newMemberAddress"
-                        label="Address"
-                        helperText="Please enter user's address"
-                        variant="standard"
-                        value={address}
-                        onChange={(e) => setAddress(e.target.value)}
                         fullWidth
+                        variant="standard"
+                        id="newMemberAddress"
+                        name="address"
+                        label="Address"
+                        value={formik.values.address}
+                        onChange={formik.handleChange}
+                        error={formik.touched.address && Boolean(formik.errors.address)}
+                        helperText={formik.touched.address && formik.errors.address}
                     />
                 </form>
             </DialogContent>
             <DialogActions className={classes.root}>
-                <Button color="primary" variant="text" size="large" onClick={handleSubmit}>
+                <Button color="primary" variant="text" size="large" onClick={formik.submitForm}>
                     Add
                 </Button>
             </DialogActions>
